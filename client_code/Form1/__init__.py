62933d672aa10f9263251efe8a76c873197ff28f
hysan110204@gmail.com <hysan110204@gmail.com>
2026-02-09 01:58:48 +0000

    Edited Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -13,7 +13,7 @@ class Form1(Form1Template):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-    self.repeating_panel_1 = 'wide'
+    self.repeating_panel_1.role = 'wide'
    
 #############################################Filter UI##############################################
     self.runner_checkbox = []
